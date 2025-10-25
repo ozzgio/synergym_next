484b6128e5444acd403e6d160d28f8909302cb49
ozzo <83029670+ozzgio@users.noreply.github.com>
2025-10-25 12:52:11 +0200

    feat(auth): Update application controller with authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  include Pundit::Authorization
+
+  helper_method :current_user_role, :user_signed_in?
+
+  protected
+
+  def current_user_role
+    current_user&.role&.to_sym
+  end
+
+  # Pundit error handling
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
